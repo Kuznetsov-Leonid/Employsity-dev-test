42ecd26328711d42a7d85762826aafdaace2288f
Kuznetsov Leonid <orlan1211@yahoo.com>
2022-12-16 11:43:23 +0300

    Header + container + navbarAPI

---
+++ app/components/hamburger/hamburger.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import styles from '../../styles/hamburger.module.scss';
+
+const Hamburger = ({children}) => {
+    const [ HamburgerOpen, SetHamburgeropen ] = useState(false);
+    let condition = HamburgerOpen;
+    return(
+        <>
+            <div 
+                className={`
+                    ${styles.Hamburger} 
+                    ${condition == true ? styles.open : styles.close}`
+                }
+                onClick = {() => condition == false ? SetHamburgeropen(true) : SetHamburgeropen(false)}
+                >
+                <div />
+                <div />
+                <div />
+            </div>
+        </>
+    );
+}
+
+export default Hamburger;
